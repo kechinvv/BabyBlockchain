94b4ade6570a934dc4f749e7bd802ebc76e4c56a
kechin.vv <kechinv1@rambler.ru>
2023-03-20 01:39:50 +0400

    update init Block for adding and creating new blocks #3

--- src/main/kotlin/valer/Blockchain.kt
+++ src/main/kotlin/valer/Blockchain.kt
@@ -6,32 +6,45 @@ import java.util.LinkedList
 object Blockchain {
     val chain = LinkedList<Block>()
 
-    data class Block(val index: Int, val prev_hash: String, val data: String) {
-        var hash = "1111"
+    data class Block(
+        val index: Int,
+        val prev_hash: String,
+        val data: String,
+        var nonce: Int? = null,
+        var hash: String? = null
+    ) {
 
         private val mainStr = index.toString() + prev_hash + data
 
         init {
-            calculateHash()
+            if (hash == null || nonce == null) {
+                nonce = 0
+                hash = "1111"
+                while (hash!!.takeLast(4) != "0000") {
+                    hash = calculateHash()
+                }
+            }
         }
 
-        private fun calculateHash() {
+        private fun verification(): Boolean {
+            var hashEqual = true
+            if (chain.isNotEmpty()) hashEqual = prev_hash == chain.last.hash
+            return (hash == calculateHash()) && hashEqual
+        }
+
+        private fun calculateHash(): String {
             val hashFunction = Hashing.sha256()
-            var nonce = 0
-            while (hash.takeLast(4) != "0000") {
-                nonce++
-                val hc = hashFunction
-                    .newHasher()
-                    .putString(mainStr + nonce.toString(), Charsets.UTF_8)
-                    .hash()
-                hash = hc.toString()
-            }
+            val hc = hashFunction
+                .newHasher()
+                .putString(mainStr + nonce.toString(), Charsets.UTF_8)
+                .hash()
+            return hc.toString()
         }
     }
 
-    fun createBlock(index: Int, prev_hash: String, data: String) {
+    fun createBlock(index: Int, prev_hash: String, data: String): Block {
         val block = Block(index, prev_hash, data)
         println(block.hash)
-        chain.add(block)
+        return block
     }
 }
