dbd0b0d9f43ee62d65ce3783027df4003a104408
kechin.vv <kechinv1@rambler.ru>
2023-03-20 07:26:19 +0400

    added verification, updated creation of block, added test with example of non-blocking block creation   #3

--- src/test/kotlin/valer/ApplicationTest.kt
+++ src/test/kotlin/valer/ApplicationTest.kt
@@ -1,5 +1,27 @@
 package valer
 
+import kotlinx.coroutines.*
+import org.junit.Test
+
 
 class ApplicationTest {
+
+    @Test
+    fun testBlock() = runBlocking {
+        var block: Blockchain.Block? = null
+        val job = launch {
+                block = Blockchain.createBlock()
+                println("finish: " + block?.hash)
+        }
+        println("creating block")
+        println("hash is:")
+        var i = 0
+        while (block == null && i < 16) {
+            delay(1)
+            println("waiting " + i++)
+            if (i == 3) {
+                job.cancel()
+            }
+        }
+    }
 }
