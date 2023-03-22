ad19dd09838c3f0ce9b199efe3b4bcce2130cb66
kechin.vv <kechinv1@rambler.ru>
2023-03-23 01:54:02 +0400

    Something working: checking and correcting chain, verification of block before adding #10

--- src/main/kotlin/valer/Application.kt
+++ src/main/kotlin/valer/Application.kt
@@ -6,25 +6,24 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 import kotlinx.coroutines.*
-import valer.Utils.distributeBlock
+import valer.Blockchain.createAddDistribute
 import valer.plugins.configureRouting
 
 var neighbors = emptyList<Int>()
+var myPort = 0
 var job: Job? = null
 fun main(): Unit = runBlocking {
     Blockchain.mode = System.getenv("NONCE_MODE") ?: "0"
     neighbors = System.getenv("NEIGHBORS").split(",").mapNotNull { it.toIntOrNull() }
-    val port = System.getenv("PORT").toInt()
+    myPort = System.getenv("PORT").toInt()
     val initNode = System.getenv("MASTER").toBoolean()
     if (initNode) job = launch(Dispatchers.Default) {
         delay(1000)
         while (isActive) {
-            val newBlock = Blockchain.createBlock()
-            Blockchain.addBlockToChain(newBlock)
-            distributeBlock(newBlock)
+            createAddDistribute()
         }
     }
-    embeddedServer(Netty, port = port, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = myPort, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
--- src/main/kotlin/valer/plugins/Routing.kt
+++ src/main/kotlin/valer/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package valer.plugins
 
+import com.google.gson.Gson
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
@@ -8,39 +9,51 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import valer.Blockchain
-import valer.Utils
+import valer.Utils.correctingChain
 import valer.job
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        get("/get_block") {
+            try {
+                val index = call.request.queryParameters["index"]!!.toInt() - 1
+                call.respondText { Gson().toJson(Blockchain.chain[index]) }
+            } catch (e: Exception) {
+                throw e
+            }
         }
 
 
         post("/add_block") {
-            val params = call.receiveParameters()
             try {
+                val params = call.receiveParameters()
                 val index = params["index"]!!.toInt()
                 val prev_hash = params["prev_hash"]
                 val data = params["data"]
                 val nonce = params["nonce"]!!.toInt()
                 val hash = params["hash"]
-                val block = Blockchain.Block(index, prev_hash!!, data!!, nonce, hash)
-                job?.cancel()
-                Blockchain.addBlockToChain(block)
-                call.respondText("ok")
-                job = launch(Dispatchers.Default) {
-                    while (isActive) {
-                        val newBlock = Blockchain.createBlock()
-                        println("MyBlock")
-                        Blockchain.addBlockToChain(newBlock)
-                        Utils.distributeBlock(newBlock)
+                try {
+                    val block = Blockchain.Block(index, prev_hash!!, data!!, nonce, hash)
+                    job?.cancel()
+                    Blockchain.addBlockToChain(block)
+                } catch (e: IllegalArgumentException) {
+                    job?.cancel()
+                    if (index > Blockchain.chain.last().index + 1) {
+                        println("I am looser")
+                        correctingChain(params["port"]!!.toInt(), index)
+                        println("It is now my blocks")
+                        println(Blockchain.chain.size)
+                        println(job!!.isActive)
+                    }
+                } finally {
+                    job = launch(Dispatchers.Default) {
+                        while (isActive) {
+                            Blockchain.createAddDistribute()
+                        }
                     }
                 }
             } catch (e: Exception) {
-                println(e.message)
-                call.respondText("er")
+                throw e
             }
         }
     }
