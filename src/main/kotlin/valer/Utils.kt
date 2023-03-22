5a8e6e74abf5aa0e1f8d0b5f9a0673261eb39ca6
kechin.vv <kechinv1@rambler.ru>
2023-03-22 22:00:22 +0400

    async distributeBlock #8

--- src/main/kotlin/valer/Utils.kt
+++ src/main/kotlin/valer/Utils.kt
@@ -5,7 +5,9 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.client.request.forms.*
 import io.ktor.client.statement.*
+import kotlinx.coroutines.coroutineScope
 import kotlin.random.Random
+import kotlinx.coroutines.*
 
 object Utils {
     private val charPool = ('a'..'z') + ('A'..'Z') + ('0'..'9')
@@ -18,21 +20,21 @@ object Utils {
         return randomString
     }
 
-    suspend fun distributeBlock(block: Blockchain.Block): Boolean {
+    suspend fun distributeBlock(block: Blockchain.Block) = coroutineScope {
         for (port in neighbors) {
-            val response = client.post("http://127.0.0.1:$port/add_block") {
-                setBody(MultiPartFormDataContent(
-                    formData {
-                        append("index", block.index)
-                        append("prev_hash", block.prev_hash)
-                        append("data", block.data)
-                        append("nonce", block.nonce!!)
-                        append("hash", block.hash!!)
-                    }
-                ))
+            async {
+                client.post("http://127.0.0.1:$port/add_block") {
+                    setBody(MultiPartFormDataContent(
+                        formData {
+                            append("index", block.index)
+                            append("prev_hash", block.prev_hash)
+                            append("data", block.data)
+                            append("nonce", block.nonce!!)
+                            append("hash", block.hash!!)
+                        }
+                    ))
+                }
             }
-            if (response.bodyAsText() == "er") return false
         }
-        return true
     }
 }
