f39fa49bad4b021ed2835041521942e21651ff80
kechin.vv <kechinv1@rambler.ru>
2023-04-23 21:25:44 +0400

    try fix callCorrecting 2 #30

--- src/test/kotlin/valer/integration/RouteTests.kt
+++ src/test/kotlin/valer/integration/RouteTests.kt
@@ -140,7 +140,7 @@ class RouteTests {
             }
             delay(1000)
             assertNotNull(jobCorrector)
-            assertTrue { jobGenerator == null || jobGenerator!!.isCancelled }
+            assertTrue {  jobGenerator?.isCancelled ?: true}
             job.cancel()
             jobGenerator?.cancel()
             jobCorrector?.cancel()
